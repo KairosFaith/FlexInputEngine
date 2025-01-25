00c3e039db26eba4db567352a92f50fe9fa80b03
Ian Ngoh <ian@mages.edu.sg>
2025-01-25 15:25:26 +0800

    Update fQuickPlay.cs
    implemented onDestroy clear object bindings

--- fQuickPlay.cs
+++ fQuickPlay.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
@@ -6,6 +7,7 @@ public class fQuickPlay : MonoBehaviour//what PlayerInput was supposed to be
     public InputActionAsset ControlAsset;
     public string ActionMap;
     InputActionMap _Map;
+    Action _ResetActions;
     void Start()
     {
         _Map = ControlAsset.FindActionMap(ActionMap);
@@ -17,6 +19,12 @@ public class fQuickPlay : MonoBehaviour//what PlayerInput was supposed to be
             a.started += d;
             a.performed += d;
             a.canceled += d;
+            _ResetActions += () =>
+            {
+                a.started -= d;
+                a.performed -= d;
+                a.canceled -= d;
+            };
         }
         EnableInput(true);
     }
@@ -26,5 +34,14 @@ public class fQuickPlay : MonoBehaviour//what PlayerInput was supposed to be
             _Map.Enable();
         else
             _Map.Disable();
+    }
+    private void OnDestroy()
+    {
+        ClearOut();
+    }
+    public void ClearOut()
+    {
+        _ResetActions?.Invoke();
+        _ResetActions = null;
     }
 }
